8adbbf6a7515dbabe36e0e654e3e200448a90f48
김근영 <kkylsi@naver.com>
2025-04-21 13:52:46 +0900

    feat: tailcss 추가

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,5 +1,5 @@
 import styles from './Home.module.scss';
-import { SearchSection } from "../components/SearchSection/SearchSection";
+import { SearchSection } from "./components/SearchSection/SearchSection";
 
 
 export interface HomeProps {
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <SearchSection />
-      <main style={{ padding: "1rem" }}>
+      <main className={`bg-yellow-100 p-4 rounded-lg ${styles.test}`}>
         <h2>인기 숙소</h2>
         {/* 카드 컴포넌트 리스트도 여기에 추가할 수 있음 */}
       </main>
