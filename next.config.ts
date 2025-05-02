21b5328b1123475a0700423c5952c77e5a1e29b7
김근영 <kkylsi@naver.com>
2025-05-02 16:44:57 +0900

    fix: nextconfig에 pwa비활성화

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,32 @@
-// next.config.ts
-import withPWA from 'next-pwa';
+// // next.config.ts
+// import withPWA from 'next-pwa';
 
-const isDev = process.env.NODE_ENV === 'development';
+// const isDev = process.env.NODE_ENV === 'development';
 
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: false,
+//   },
+//   experimental: {
+//     typedRoutes: true,
+//   },
+//   pwa: { // pwa 설정을 nextConfig 내부로 이동
+//     dest: 'public',
+//     disable: isDev,
+//     register: true,
+//     skipWaiting: true,
+//     fallbacks: {
+//       document: '/offline.html',
+//     },
+//   },
+// };
+
+// export default withPWA(nextConfig);
+
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: false,
@@ -11,15 +34,6 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-  pwa: { // pwa 설정을 nextConfig 내부로 이동
-    dest: 'public',
-    disable: isDev,
-    register: true,
-    skipWaiting: true,
-    fallbacks: {
-      document: '/offline.html',
-    },
-  },
-};
+}
 
-export default withPWA(nextConfig);
+export default nextConfig
